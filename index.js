b3dfec870a8dc834f37f682642630de358fa13c9
bdrencher <bdrencher@gmail.com>
2020-08-29 14:49:14 -0400

    adding a get route for projects

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const projectController = require('./server/controller/projectController');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+app.get("/projects", (res) => {
+    projectController.getProjects(res)
+});
