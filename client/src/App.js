57e6fb6a8cce39eb357714c9e85e219794ebece9
bdrencher <bdrencher@gmail.com>
2020-08-30 02:20:20 -0400

    overhauling the projects component to fetch data from server

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,10 @@ import React from 'react';
 import './App.css';
 import Project from './Components/projects';
 
-const name = "name";
-const description = "description";
-const github = "www.github.com";
-const url = "www.heroku.com";
-const tech = ["React", "Nodejs"];
-
-const testProject = <Project name={"name"} description={"description"}
-github={"www.github.com"} url={"www.heroku.com"} tech={["React", "Nodejs"]}/>
 function App() {
   return (
     <div className="App">
-      <Project name={name} description={description} github={github} url={url} tech={tech}/>
+      <Project />
     </div>
   );
 }
--- client/src/Components/projects.js
+++ client/src/Components/projects.js
@@ -1,26 +1,78 @@
 import React from 'react';
 
-const Project = props => {
-    const name = props.name;
-    const description = props.description;
-    const github = props.github;
-    const url = props.url;
-    const tech = props.tech.map((techName) => {
-        return (
-        <li>{techName}</li>
-        );
-    });
-    return (
-        <div className="project">
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <a href={github}>GitHub</a>
-            <a href={url}>Project demonstration</a>
-            <p>This project uses the following technologies:</p>
-            <ul>{tech}</ul>
-            IT WORKED!!!!
-        </div>
-    )
+class Project extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("/projects")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    projects: result.projects
+                });
+            },
+            (error) => {
+                console.log("An error occured getting project data");
+                this.setState({
+                    projects: null
+                });
+            }
+        )
+    }
+
+    render() {
+        const { projects } = this.state;
+        if (projects == null) {
+            return (
+                <div>
+                    An error occurred fetching project data
+                </div>
+            );
+        } else {
+            return (
+                <div className="projects">
+                    {projects.map((project, index) => (
+                        <div className={index}>
+                            <h3>{project.name}</h3>
+                            <p>{project.description}</p>
+                            <a href={project.github}>Github</a>
+                            <a href={project.url}>Project demonstration</a>
+                            {/* add tech at some point */}
+                        </div>  
+                    ))}
+                </div>
+            );
+        }
+    }
 }
+
+// const Project = props => {
+//     const name = props.name;
+//     const description = props.description;
+//     const github = props.github;
+//     const url = props.url;
+//     const tech = props.tech.map((techName) => {
+//         return (
+//         <li>{techName}</li>
+//         );
+//     });
+//     return (
+//         <div className="project">
+//             <h3>{name}</h3>
+//             <p>{description}</p>
+//             <a href={github}>GitHub</a>
+//             <a href={url}>Project demonstration</a>
+//             <p>This project uses the following technologies:</p>
+//             <ul>{tech}</ul>
+//             IT WORKED!!!!
+//         </div>
+//     )
+// }
 
 export default Project;
