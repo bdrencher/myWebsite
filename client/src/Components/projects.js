74fea82cc6d6696df33cfe1934ce76486d545fc2
bdrencher <bdrencher@gmail.com>
2020-08-29 20:30:43 -0400

    adding a project component for rendering projects

---
+++ client/src/Components/projects.js
@@ -0,0 +1,24 @@
+class Project extends React.Component {
+    constructor(iName, iDescription, iGithub, iURL, iTechnologies) {
+        super();
+        this.name = iName;
+        this.description = iDescription;
+        this.github = iGithub;
+        this.url = iURL;
+        this.tech = iTechnologies;
+        this.techList = this.tech.map((tech) => <li>{tech}</li>);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.name}</h3>
+                <p>{this.description}</p>
+                <a href={this.github}>GitHub</a>
+                <a href={this.url}>Project demonstration</a>
+                <p>This project uses the following technologies:</p>
+                <ul>{this.techList}</ul>
+            </div>
+        )
+    }
+}
