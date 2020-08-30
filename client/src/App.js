377566e8ad4d39c41e191fa42234035d8f1a674a
bdrencher <bdrencher@gmail.com>
2020-08-29 21:27:24 -0400

    attempting to add property values to Project component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import Project from './Components/projects';
 
-const testProject = <Project name={"name"}/>
+const testProject = <Project name={"name"} description={"description"}
+github={"www.github.com"} url={"www.heroku.com"} tech={["React", "Nodejs"]}/>
 function App() {
   return (
     <div className="App">
