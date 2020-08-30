2a7cb9d075d50d141c1903fcb3806480217a7ffa
bdrencher <bdrencher@gmail.com>
2020-08-29 21:22:48 -0400

    trying to get an instance of my project component on screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 import Project from './Components/projects';
 
-const testProject = new Project("Test project", "IDK, this is just a test",
-"www.github.com", "www.heroku.com", ["Node.js", "React", "Alexa"]);
-
+const testProject = <Project name={"name"}/>
 function App() {
   return (
     <div className="App">
