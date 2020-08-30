424dcae576bc5bcc04bcf006b7f56b082316ab1c
bdrencher <bdrencher@gmail.com>
2020-08-30 02:30:34 -0400

    adding some quick place holder text

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,21 @@ import Project from './Components/projects';
 function App() {
   return (
     <div className="App">
+      <h1>Ben Rencher</h1>
+      <p>
+        Welcome to my portfolio site! As you can see, 
+        it is still under construction. If you would like
+        to see more information about my projects, please
+        visit my GitHub page: 
+      </p>
+      <a href="https://github.com/bdrencher">GitHub</a>
+      <p>. </p>
+      <p>
+        More information about me can be found on LinkedIn: 
+      </p>
+      <a href="https://www.linkedin.com/in/ben-rencher-250114b3/">LinkedIn</a>
+      <p>.</p>
+
       <Project />
     </div>
   );
