94ed5d81d30e3b8e41f57e003b3d746a3ff2ad7e
bdrencher <bdrencher@gmail.com>
2020-08-29 21:11:41 -0400

    removing default stuff and adding test project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Project from './Components/projects';
+
+const testProject = new Project("Test project", "IDK, this is just a test",
+"www.github.com", "www.heroku.com", ["Node.js", "React", "Alexa"]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {testProject}
     </div>
   );
 }
