54e7da17ec369f85077ebce94e4d1e616015f01d
bdrencher <bdrencher@gmail.com>
2020-10-16 15:17:04 -0400

    importing the Card library from react bootstrap

--- client/src/Components/projects.js
+++ client/src/Components/projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './projects.css';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 class Project extends React.Component {
     constructor(props) {
