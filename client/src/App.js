f5ee5650f837ce4c50b7fa6c5288d6e9b06474d6
bdrencher <bdrencher@gmail.com>
2020-10-16 16:18:02 -0400

    removing jumbotron since it isn't styled easily

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Project from './Components/projects';
 
 function App() {
   return (
     <div className="App">
-      <Jumbotron className="jumbotron">
+      <div className="jumbotron">
         <h1>Ben Rencher</h1>
         <p>
           Welcome to my portfolio site! As you can see, 
@@ -16,7 +15,7 @@ function App() {
           visit my GitHub page: <a href="https://github.com/bdrencher">GitHub</a>. 
           More information about me can be found on LinkedIn: <a href="https://www.linkedin.com/in/ben-rencher-250114b3/">LinkedIn</a>.
         </p>
-      </Jumbotron>
+      </div>
       <Project />
     </div>
   );
